cbdb1f4a66d545c3c8963324974d9a80aad39a96
amitgupta1324 <amitgupta321100@gmail.com>
2024-12-30 18:31:37 +0530

    commit code

---
+++ utils/jwtToken.js
@@ -0,0 +1,18 @@
+export const sendToken = (user, statusCode, res, message) => {
+  const token = user.getJWTToken();
+
+  // Adding default fallback for COOKIE_EXPIRE
+  const options = {
+    expires: new Date(
+      Date.now() + (process.env.COOKIE_EXPIRE || 7) * 24 * 60 * 60 * 1000 // Default to 7 days if undefined
+    ),
+    httpOnly: true,
+  };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    user,
+    message,
+    token,
+  });
+};
