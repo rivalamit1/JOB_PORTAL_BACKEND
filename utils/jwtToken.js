4d60356536d5c7da26d78a2c41113eb924a7a7b7
Amit Gupta <125515651+amitgupta1324@users.noreply.github.com>
2025-01-20 10:50:17 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,7 +2,7 @@ export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
 
   // Adding default fallback for COOKIE_EXPIRE
-  const options = {
+  const options = { 
     expires: new Date(
       Date.now() + (process.env.COOKIE_EXPIRE || 7) * 24 * 60 * 60 * 1000 // Default to 7 days if undefined
     ),
