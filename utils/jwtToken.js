7fc164f37d938cf0f57aa6526683a3427f0c2fd0
Amit Gupta <125515651+amitgupta1324@users.noreply.github.com>
2025-02-17 09:52:19 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,7 +7,7 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + (process.env.COOKIE_EXPIRE || 7) * 24 * 60 * 60 * 1000 // Default to 7 days if undefined
     ),
     httpOnly: true,
-    secure: true,
+    secure: true, 
     sameSite: "none"
   };
 
