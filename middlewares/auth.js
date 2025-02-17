6b4b1a090ba8981a5bf7608fa78437b0396b0324
Amit Gupta <125515651+amitgupta1324@users.noreply.github.com>
2025-02-17 09:53:02 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ import { User } from "../models/userSchema.js";
  
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
-  if (!token) {
+  if (!token) { 
     return next(new ErrorHandler("User is not authenticated.", 400));
   }
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
