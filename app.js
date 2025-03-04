e142fef45cfec627160794d3604ade6f270150f0
Amit Gupta <125515651+rivalamit1@users.noreply.github.com>
2025-03-04 12:02:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ config({ path: "./config/config.env" });
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"], 
     credentials: true,
   })
 );
