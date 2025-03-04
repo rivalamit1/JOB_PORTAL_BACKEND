06727b86fb1e9b542bcad93c4e7fa28c22ceb0fb
Amit Gupta <125515651+rivalamit1@users.noreply.github.com>
2025-03-04 12:04:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import { newsLetterCron } from "./automation/newsLetterCron.js";
   
 const app = express();  
 config({ path: "./config/config.env" });
-
+ 
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
